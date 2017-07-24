df130e812a2ed98e9545eb1fb28500fb2848f677
amart90 <anthonymartinez90@gmail.com>
2017-07-24 13:31:47 -0700

    Minor: gitignore

--- Scripts/ScoringFunctions.R
+++ Scripts/ScoringFunctions.R
@@ -11,5 +11,11 @@ FIso <- L/(1+A*exp(-k*x))
 # Plot
 plot(x, FIso)
 
+r <- raster("D:/antho/Downloads/LC08_L1TP_045027_20170723_20170723_01_RT_TIR.jpg")
+e <- extend(r, PNW, value=0)
+r <- readOGR("D:/antho/Desktop/UIdaho/Datasets/Table Mountain/Beaird_e.shp")
 
-
+plot(e, add=T)
+plot(PNW)
+q <- rasterize(r, p, fun=sum)
+extend()
